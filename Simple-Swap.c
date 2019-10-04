23b7d79f7cb278385465ee21043a6775cf283dc5
Joseph Fergen <josephfergen150@gmail.com>
2019-10-04 12:19:49 -0500

    Add files via upload

---
+++ Simple-Swap.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int* int_ptr;
+    int_ptr = (int*) malloc(2 * sizeof(int));
+
+    if (int_ptr == NULL) {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+
+    printf("Enter first integer: ");
+    scanf("%d", &int_ptr[0]);
+
+    printf("Enter second integer: ");
+    scanf("%d", &int_ptr[1]);
+
+    printf("Original values: 1st =\t%d 2nd =\t%d\n", int_ptr[0], int_ptr[1]);
+
+    int_ptr[0] = int_ptr[0] ^ int_ptr[1];
+    int_ptr[1] = int_ptr[0] ^ int_ptr[1];
+    int_ptr[0] = int_ptr[0] ^ int_ptr[1];
+
+    printf("Swapped values: 1st =\t%d 2nd =\t%d\n", int_ptr[0], int_ptr[1]);
+    free(int_ptr);
+
+    return 0;
+}
